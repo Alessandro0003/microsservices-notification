814bbfc742f69fa5b4224340653420c5217c771b
Alessandro0003 <alessandroquinta3@gmail.com>
2024-06-15 23:27:51 -0300

    🔥 feat: creating method to search notifications

---
+++ src/application/uses-cases/get-recipient-notifications.ts
@@ -0,0 +1,26 @@
+import { NotificationsRepository } from '@application/repositories/notification-repository';
+import { Injectable } from '@nestjs/common';
+import { Notification } from '@application/entities/notification';
+
+interface GetRecipientNotificationsRequest {
+  recipientId: string;
+}
+
+interface GetRecipientNotificationResponse {
+  notifications: Notification[];
+}
+
+@Injectable()
+export class GetRecipientNotification {
+  constructor(private notificationRepository: NotificationsRepository) {}
+
+  async execute(request: GetRecipientNotificationsRequest): Promise<GetRecipientNotificationResponse> {
+    const { recipientId } = request;
+
+    const notifications = await this.notificationRepository.findManyByRecipientId(recipientId);
+
+    return {
+      notifications,
+    };
+  }
+}
